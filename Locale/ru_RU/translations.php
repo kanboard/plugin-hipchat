727386243217e433942603c5d2bb2d431101fe52
Dmitry <kochevd@gmail.com>
2016-01-03 02:10:29 +0300

    Update translations.php

--- Locale/ru_RU/translations.php
+++ Locale/ru_RU/translations.php
@@ -2,7 +2,7 @@
 
 return array(
     'Room API ID or name' => 'API ID комнаты или имя',
-    'Room notification token' => 'Ключь комнаты для уведомлений',
+    'Room notification token' => 'Ключ комнаты для уведомлений',
     'Help on Hipchat integration' => 'Помощь по интеграции Hipchat',
 );
 
