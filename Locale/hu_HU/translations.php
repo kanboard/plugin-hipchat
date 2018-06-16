088c884ec1d44046beee9bcfbda84172f6d4e638
Balázs Úr <urbalazs@gmail.com>
2018-06-16 17:17:51 +0200

    Updated Hungarian translation

--- Locale/hu_HU/translations.php
+++ Locale/hu_HU/translations.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    // 'Room API ID or name' => '',
-    // 'Room notification token' => '',
-    // 'Help on Hipchat integration' => '',
+    'Room API ID or name' => 'Szoba API azonosító vagy név',
+    'Room notification token' => 'Szoba értesítési token',
+    'Help on Hipchat integration' => 'Súgó a Hipchat integrációhoz',
 );
 
